24aaf4b5014f2c29b9652c4bb399504d630b381b
Khalil <khalil.ciret@laposte.net>
2015-11-17 00:09:42 +0100

    Added the progress bar and linked it to the buttons.

--- app/src/main/java/com/liquidwolf/drinksomemore/HomeActivity.java
+++ app/src/main/java/com/liquidwolf/drinksomemore/HomeActivity.java
@@ -4,9 +4,12 @@ package com.liquidwolf.drinksomemore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     /**
@@ -18,14 +21,48 @@ public class HomeActivity extends AppCompatActivity {
     private static final int LITTLE_BOTTLE = 50;
     private static final int REGULAR_BOTTLE = 100;
 
+    private int waterToday;
+    private SeekBar barWater;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
+
+        this.barWater = (SeekBar) this.findViewById(R.id.seekBar);
+        barWater.setMax(300); //TODO Replace 300 by a variable or a constant
+
     }
+
+    public void onClick (View v) {
+
+      switch (v.getId()) {
+
+          case (R.id.button26):
+              this.waterToday += LITTLE_GLASS;
+              break;
+          case (R.id.button33):
+              this.waterToday += REGULAR_GLASS;
+              break;
+          case R.id.button50:
+              this.waterToday += LITTLE_BOTTLE;
+              break;
+          case R.id.button100:
+              this.waterToday += REGULAR_BOTTLE;
+              break;
+      }
+
+        this.refreshBar();
+
+
+      }
+
+    public void refreshBar(){
+            this.barWater.setProgress(this.waterToday);
+
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
